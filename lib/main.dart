d1443767bc6e67b5da6af3b17a12765a6a6717f7
mondel123 <adamsmonday1@gmail.com>
2024-03-16 07:25:32 +0100

    removed the widget

--- lib/main.dart
+++ lib/main.dart
@@ -58,15 +58,6 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
-
 class BigCard extends StatelessWidget {
   const BigCard({
     super.key,
